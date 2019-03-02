92a3482ea2fccbe6d52630b46d6569b3b569fe51
Umesh1979 <bhatnagarumesh@gmail.com>
2019-03-02 11:46:43 +0530

    Initial commit

---
+++ app/src/main/java/com/ub/youtubeintegration_assignment/Config.java
@@ -0,0 +1,10 @@
+package com.ub.youtubeintegration_assignment;
+
+public final class Config
+{
+    private Config()
+    {
+
+    }
+    public static final String YOUTUBE_API_KEY="AIzaSyB6joqSTFJ4TXJ4DFbjODvAPCkk7NX7MNs";
+}
